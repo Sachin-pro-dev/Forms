018715630bb8d70dd41724818cefcd7eaa9e199d
Sachin <sachinbaluragi2.0@gmail.com>
2024-10-26 00:48:33 +0530

    added responsiveness to the tables

--- client/src/app/(Pages)/Print/[form_no]/page.jsx
+++ client/src/app/(Pages)/Print/[form_no]/page.jsx
@@ -304,7 +304,7 @@ const Page = () => {
           />
         </div>
 
-        <h3>Details of Student's Bank</h3>
+        <h3>Details of Student&apos;s Bank</h3>
         <div className="form-group">
           <label htmlFor="bankAccountName">Name in the Bank Account:</label>
           <input
@@ -386,7 +386,7 @@ const Page = () => {
         </div>
         <div className="form-group">
           <label htmlFor="collegeName">
-            Present C.A. Firm's Name / College Name:
+            Present C.A. Firm&apos;s Name / College Name:
           </label>
           <input
             type="text"
